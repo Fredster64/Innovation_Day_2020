06ebbaa51fcb3cd174c20a4609c8fa056d6ecd01
Freddie Payne <Freddie.Payne@ghyston.com>
2020-10-29 16:28:08 +0000

    Updated some old names

--- slagheap/Program.cs
+++ slagheap/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace News_Emailer
+namespace slagheap
 {
     public class Program
     {
--- slagheap/Startup.cs
+++ slagheap/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace News_Emailer
+namespace slagheap
 {
     public class Startup
     {
